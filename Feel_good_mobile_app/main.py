2983eba5855b555714035389c1a866378f9905b6
rexaevum <rolans2k@gmail.com>
2020-12-23 19:52:22 -0500

    user to change password if they forgot it; json.load needs to be wraped in a try

--- Feel_good_mobile_app/main.py
+++ Feel_good_mobile_app/main.py
@@ -1,10 +1,13 @@
 # Necessary imports for connecting with kivy
+import json, glob, random, time
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json, glob, random
 from datetime import datetime
 from pathlib import Path
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.button import ButtonBehavior
 # load a kivy file for the ui design
 Builder.load_file('design.kv')
 filePath = r"data\users.json"
@@ -16,33 +19,46 @@ class LoginScreen(Screen): # Inhereting the Screen object
     def sign_up(self):
         # To navigat to another screen
         self.manager.current = "signup_screen" # Need to match the .kv file root widget name
-    
+    def forgot(self):
+        self.manager.current = 'forgot_password'
 
     def login(self, user, passw):
         with open(filePath) as file:
-            data = json.load(file)
-            if user in data and data[user]['password'] == passw: # Contains
-                self.manager.transition.direction = 'left'
-                self.manager.current = "login_screen_success"
-            else:
-                self.ids.loginFailed.text = "Incorrect User Info"
+            try:
+                data = json.load(file)                            
+                if user in data and data[user]['password'] == passw: # Contains
+                    self.manager.transition.direction = 'left'
+                    self.manager.current = "login_screen_success"
+                else:
+                    self.ids.loginFailed.text = "Incorrect User Info"
+            except json.decoder.JSONDecodeError:
+                self.ids.loginFailed.text = "No Users Registerd"
+
 
 
 # Every screen/page has to have its own name with the class name matching the .kv file
 class SignupScreen(Screen):
     def add_user(self, uname, pword):
         # Get info from .json file
+        self.users = None
         with open(filePath, 'r') as file:
-            users = json.load(file)
+            try:
+                self.users = json.load(file)
+            except:
+                pass
         # Add the new info to the db
-        users[uname] = {'username' : uname, 'password' : pword, 
+        self.users[uname] = {'username' : uname, 'password' : pword, 
         'created' : datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
-        print(users) 
         # Overwrite the new users to the file
         with open(filePath, 'w') as file:
-            json.dump(users, file)
+            json.dump(self.users, file)
         # move to the success screen
         self.manager.current = "signup_screen_success"
+            
+
+    
+    def go_back(self):
+        self.manager.current = 'login_screen'
 
 class SignupScreenSuccess(Screen):
     def back(self):
@@ -72,6 +88,41 @@ class LoginScreenSuccess(Screen):
             self.ids.toInspire.text = random.choice(quotes)
         else:
             self.ids.toInspire.text = "Sorry, have nothing for this. Please try another emotion."
+
+class ForgotPasswordScreen(Screen):
+    def change_password(self, username, pass1, pass2):
+        print((username, pass1, pass2))
+        with open(filePath, 'r') as file:
+            data = json.load(file)
+            if username in data:
+                if pass1 == pass2:
+                    data[username]['password'] = pass1
+                    self.write_json(data)
+                    self.ids.password_status.text = "Password Changed"
+                    time.sleep(2)
+                    self.manager.current = 'login_screen'
+                else:
+                    self.ids.password_status.text = 'Passwords did not match'
+            else:
+                self.ids.password_status.text = 'User not found. Make sure to sign up first'
+        
+    def write_json(self, data):
+        with open(filePath, 'w') as file:
+            try:
+                json.dump(data, file)
+            except:
+                print('Failed to update db')
+
+    def moveto_sighnup(self):
+        self.manager.current = 'signup_screen'
+
+
+# Creating an interactive button as an image
+# ButtonBehavior has to come first
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
+
+
 # RootWidget is after the App in the hierarchy
 class RootWidget(ScreenManager):
     pass
@@ -79,6 +130,7 @@ class RootWidget(ScreenManager):
 # Highes lvl in the app hierarchy
 class MainApp(App):
     def build(self):
+        self.title = "Feel Good"
         return RootWidget()
 
 # If this file is the main file, run the application
