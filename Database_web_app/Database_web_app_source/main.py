635877e3d1eef6bb64317c77d0e8e31e1b75cfba
rexaevum <rolans2k@gmail.com>
2020-12-30 00:48:16 -0500

    implement finished app; customise the email message by adding avg data and nr of participants

--- Database_web_app/Database_web_app_source/main.py
+++ Database_web_app/Database_web_app_source/main.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template, request
+"""
+    This web app will allow users to enter their email, height and age, which will be added to a database. if the entry is accepted (not duplicat)
+    the user will receive a dynamicaly generated email that shows the values they have entered, as well as the average height and age
+    of all the survery participants.
 
+    Used technology: Flask, SQLAlchemy, postgres2
+"""
+import flask
+from flask import Flask, render_template, request
+# For SQLAlchemy to work with postgres, need to install posgres2
+from flask_sqlalchemy import SQLAlchemy 
+from send_email import send_email # script will send email
+from sqlalchemy.sql import func # to querry averages
+
+# Create a flask app and connecting it to a db
 app= Flask(__name__)
+#URI ==> {server used}://{username}:{password}@{server IP}/{DB Name}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin2020@localhost:5432/STATISTICS'
+db = SQLAlchemy(app)
+
+# create a table in the db
+class Data(db.Model):
+    __tablename__ = 'user_info'
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(150), unique=True)
+    height_ = db.Column(db.Integer)
+    age_ = db.Column(db.Integer)
+
+    def __init__(self, email, height, age):
+        self.email_ = email
+        self.height_ = height
+        self.age_ = age
+
 
 @app.route('/') # by efault this is a GET method
 def index():
@@ -9,15 +39,40 @@ def index():
 @app.route('/success/', methods=['POST']) # if you want to specify another method
 def success():
     if request.method == 'POST':
-        print(request.form)
+        #print(request.form)
+        # get values from 
         email = request.form['userEmail'] # the defined name= variable in html is what you add here
         height = request.form['userHeight']
         age = request.form['userAge']
-    return render_template('got_data.html')
+        # make sure no duplicates by counting instances of email in the db
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            # FIXME - important
+            # create an instance of our table object
+            data = Data(email, height, age)
+            # add to db
+            db.session.add(data)
+            # commit to db
+            db.session.commit()
+
+            # get avg height and age from db
+            avgHeight = db.session.query(func.avg(Data.height_)).scalar() # generates sql statement without .scalar()
+            avgAge = db.session.query(func.avg(Data.age_)).scalar()
+            # Format float
+            avgHeight = round(avgHeight, 1)
+            avgAge = round(avgAge, 1)
+            # Get participant count
+            participants = db.session.query(Data).count()
+            # send the email
+            send_email(email, height, age, avgHeight, avgAge, participants)
+            # if everything went ok, move to the got_data.html screen
+            return render_template('got_data.html')
+        return render_template('index.html',
+         text="Email address already registered. Please use a different email.")
 
 
 
 # Check if file is main (the executed script)    
 if __name__=='__main__':
     app.debug = True
+    #db.create_all() #only needed first time
     app.run(port=5000)
---
+++ Database_web_app/Database_web_app_source/send_email.py
@@ -0,0 +1,25 @@
+from email.mime.text import MIMEText # to read a sting as html
+import smtplib
+
+def send_email(email, height, age, avgHeight, avgAge, participants):
+    from_email = 'rexaevum.dev@gmail.com'
+    from_password = 'admin2020'
+    to_email = email
+
+    subject = 'Data'
+    message = "Your height is <strong>{}</strong> and age is <strong>{}</strong>.".format(height, age)
+    message = message + "From the <strong>{}</strong> survey participants, the current average height is ".format(participants)
+    message = message +  "<strong>{}</strong> and the average age is <strong>{}</strong>".format(avgHeight, avgAge)
+
+    # Covnert to a html
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = subject
+    msg['To'] = to_email
+    msg['From'] = from_email
+
+    # Connect to from_email
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email, from_password)
+    gmail.send_message(msg)
